244ba8558d84896c00e5f903116941f3d5db15ff
manystorms <kimhayul2007@naver.com>
2024-05-13 12:22:37 +0900

    barometer_test

--- lib/screen.dart
+++ lib/screen.dart
@@ -5,38 +5,98 @@ import 'package:mud_safety/get_gps.dart';
 import 'package:mud_safety/get_height.dart';
 import 'package:mud_safety/get_pressure.dart';
 
-class MyApp extends StatelessWidget {
-  Future<void> getPressure() async {
-    WidgetsFlutterBinding.ensureInitialized();
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_barometer/flutter_barometer.dart';
 
-    // 현재 압력 값을 가져오기 위해 currentPressure 메서드 호출
-    try {
-      BarometerValue pressureValue = await FlutterBarometer.currentPressure;
-      // pressureValue에서 필요한 정보를 추출하여 사용
-      print('Current Pressure in hectopascal: ${pressureValue.hectpascal}');
-      print('Current Pressure in inch of Mercury: ${pressureValue.inchOfMercury}');
-      print('Current Pressure in millimeter of Mercury: ${pressureValue.millimeterOfMercury}');
-      print('Current Pressure in pounds per square inch: ${pressureValue.poundsSquareInch}');
-      print('Current Pressure in atmospheres: ${pressureValue.atm}');
-    } catch (error) {
-      // 오류 처리
-      print('Error fetching current pressure: $error');
-    }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  BarometerValue _currentPressure = BarometerValue(0.0);
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+    FlutterBarometer.currentPressureEvent.listen((event) {
+      setState(() {
+        _currentPressure = event;
+      });
+    });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
+    try {
+      platformVersion =
+          await FlutterBarometer.platformVersion ?? 'Unknown platform version';
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Button Example'),
+          title: const Text('flutter_barometer'),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              future: getPressure();
-            },
-            child: Text('Press Me'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Running on: $_platformVersion\n',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '${(_currentPressure.hectpascal * 1000).round() / 1000} hPa',
+                style: TextStyle(
+                  fontSize: 70,
+                ),
+              ),
+              Text(
+                '${(_currentPressure.inchOfMercury * 1000).round() / 1000} inHg',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '${(_currentPressure.millimeterOfMercury * 1000).round() / 1000} mmHg',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '${(_currentPressure.poundsSquareInch * 1000).round() / 1000} psi',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                '${(_currentPressure.atm * 1000).round() / 1000} atm',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ],
           ),
         ),
       ),
